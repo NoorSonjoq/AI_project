f9981da7c13cbf4ac619a4e329a460206ac2b126
NoorSonjoq <noorsonjoq97@gmail.com>
2025-10-14 15:51:48 +0300

    project

--- client/frontend/src/pages/auth/Login.jsx
+++ client/frontend/src/pages/auth/Login.jsx
@@ -42,7 +42,7 @@ export default function Login() {
     e.preventDefault();
 
        try {
-            const response = await axios.post(`${API_URL}/api/auth/login`, userValues)
+            const response = await axios.post(`${API_URL}/auth/login`, userValues)
             if (response.status === 200 && response.data.success) {
     localStorage.setItem('token', response.data.token);
     setSuccess('Login successful, Redirecting...');
