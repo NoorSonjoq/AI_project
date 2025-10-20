0e0e2ecbbddda1c979680436cbb16c5ec32d0bab
NoorSonjoq <noorsonjoq97@gmail.com>
2025-10-20 14:28:15 +0300

    project

--- server/src/services/historyService.js
+++ server/src/services/historyService.js
@@ -1,9 +1,23 @@
+// import UserReportHistory from "../models/historyModel.js";
+
+// export const createHistory = async (user_id, report_id = null, action_title) => {
+//   try {
+//     await UserReportHistory.create({ user_id, report_id, action_title });
+//   } catch (err) {
+//     console.error("Failed to create history:", err);
+//   }
+// };
+
+
 import UserReportHistory from "../models/historyModel.js";
 
 export const createHistory = async (user_id, report_id = null, action_title) => {
   try {
-    await UserReportHistory.create({ user_id, report_id, action_title });
+    const history = await UserReportHistory.create({ user_id, report_id, action_title });
+    console.log("History saved:", history.history_id || history.id);
+    return history; // ارجاع التاريخ للتاكد
   } catch (err) {
     console.error("Failed to create history:", err);
+    throw err; // رفع الخطأ ليتعرف عليه الكود الرئيسي
   }
 };
